71d12a07e815b684741ab3330ae85ac8ce123d1b
Abdelkader-Jerbi <Abdelkader-Jerbi@esprit.tn>
2025-12-24 12:12:14 +0100

    Remove lite version

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -3,5 +3,3 @@ config();
 
 import '@/ai/flows/score-calculator.ts';
 import '@/ai/flows/plain-language-analysis.ts';
-import '@/ai/flows/plain-language-analysis-lite.ts';
-import '@/ai/flows/plain-language-analysis-superlite.ts';
